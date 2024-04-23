58a0f566e57d502ddfebde831983928cd2faffd5
k2fa <khalidarrafi6331@gmail.com>
2024-04-23 14:22:03 +0700

    style (tfn chang) - styling for table tfn chang

--- resources/js/Components/FillTable/ChangFill.jsx
+++ resources/js/Components/FillTable/ChangFill.jsx
@@ -1,9 +1,48 @@
 import React from "react";
 
-export default function ChangFill(){
+export default function ChangFill() {
     return (
         <>
-            <h1>Cek</h1>
+            <table className="items-center w-full bg-transparent border-collapse">
+                <thead>
+                    <tr>
+                        <th className="px-4 sm:px-6  sm:w-4 align-middle border border-solid py-3 text-sm sm:text-base  border-slate-300 whitespace-nowrap font-semibold text-center text-blueGray-700">
+                            No
+                        </th>
+                        <th className="px-4 sm:px-6 align-middle border border-solid py-3 text-sm sm:text-base  border-slate-300 whitespace-nowrap font-semibold text-left text-blueGray-700">
+                            Intensitas Kepentingan
+                        </th>
+                        <th className="px-4 sm:px-6 align-middle border border-solid py-3 text-sm sm:text-base  border-slate-300 whitespace-nowrap font-semibold text-left text-blueGray-700">
+                            Himpunan Linguistik
+                        </th>
+                        <th className="px-4 sm:px-6 align-middle border border-solid py-3 text-sm sm:text-base  border-slate-300 whitespace-nowrap font-semibold text-left text-blueGray-700">
+                            Triangular Fuzzy Number
+                        </th>
+                        <th className="px-4 sm:px-6 align-middle border border-solid py-3 text-sm sm:text-base  border-slate-300 whitespace-nowrap font-semibold text-left text-blueGray-700">
+                            Reciprocal
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="px-4 sm:px-6 align-middle border border-solid py-3 text-sm text-blueGray-500 border-slate-300 whitespace-nowrap text-center font-semibold">
+                            1
+                        </td>
+                        <td className="px-4 sm:px-6 align-middle border border-solid py-3 text-sm text-blueGray-500 border-slate-300 whitespace-nowrap text-center font-semibold">
+                            1
+                        </td>
+                        <td className="px-4 sm:px-6 align-middle border border-solid py-3 text-sm text-blueGray-500 border-slate-300 whitespace-nowrap text-left font-semibold uppercase">
+                            cek
+                        </td>
+                        <td className="px-4 sm:px-6 align-middle border border-solid py-3 text-sm text-blueGray-500 border-slate-300 whitespace-nowrap text-center font-semibold uppercase">
+                            (1, 1, 1)
+                        </td>
+                        <td className="px-4 sm:px-6 align-middle border border-solid py-3 text-sm text-blueGray-500 border-slate-300 whitespace-nowrap text-center font-semibold uppercase">
+                            (1, 1, 1)
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </>
-    )
+    );
 }
